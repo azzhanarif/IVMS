0cc399ff95649231f3b015e0897bcf022f36bb85
azzhanarif <azzhanarifsat@gmail.com>
2025-11-22 19:55:01 +0500

    Update ivms.cpp

---
+++ ivms.cpp
@@ -0,0 +1,159 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+using namespace std;
+struct Item {
+    int id;
+    string name;
+    int quantity;
+    float price;
+    string category;
+};
+
+Item inventory[100];
+
+void addItem(int& itemCount)
+{
+    cout << "****************************************** ADDING ITEMS ******************************************" << endl;
+
+
+    if (itemCount > 100) {
+        cout << "\nInventory full! delete previous items to store new ones\n";
+        return;
+    }
+
+    cout << "Enter the name of item: ";
+    cin.ignore();
+    getline(cin, inventory[itemCount].name);
+
+    cout << "Enter the quantity of item: ";
+    cin >> inventory[itemCount].quantity;
+
+    cout << "Enter item id: ";
+    cin >> inventory[itemCount].id;
+
+    cout << "Enter price of the item: ";
+    cin >> inventory[itemCount].price;
+
+    cout << "Enter category of the item: ";
+    cin.ignore();
+    getline(cin, inventory[itemCount].category);
+    
+
+    itemCount++;
+    
+    cout << "Item " << itemCount << " added successfully! " << endl;
+
+
+
+}
+
+void removeItem()
+{
+
+}
+
+
+int menu()
+{
+    int choice;
+    cout << "Menu is below: \n";
+    cout << "Press (1) to add items: \n";
+    cout << "Press (2) to delete items: \n";
+    cout << "Press (3) to edit items: \n";
+    cout << "Press (4) to view items: \n";
+    cout << "Press (5) to Exit: \n";
+
+    cout << "Your coice: ";
+
+    do
+    {
+  
+            if (cin >> choice)
+            {
+                if (choice > 5 || choice < 1) {
+                    cout << "Invalid Input!\n";
+                    continue;
+                }
+                break;
+            }
+            else
+                {
+                cin.clear();
+                cin.ignore();
+                cout << "Invalid Input!\n";
+                continue;
+            }
+
+    } while (true);
+
+    return choice;
+} 
+
+int main()
+{
+    cout << "****************************************** INVENTORY MANAGEMENT ******************************************\n\n" << endl;
+
+    int itemCount = 0;
+
+    while (true)
+    {
+        switch (menu())
+        {
+        case 1:
+        {
+            int k;
+            while (true) {
+                addItem(itemCount);
+                cout << "Do you want to continue adding items or exit to menu? \n";
+                cout << "Press (1) for continue adding items and press (2)  to exit to main menu \n";
+                 
+                do {
+
+                    if (cin >> k)
+                        break;
+                    else
+                    {
+                        cin.clear();
+                        cin.ignore();
+                        cout << "Invalid Input!\n";
+                        continue;
+                    }
+
+
+                } while (true);
+
+                if (k == 1)
+                {
+                    continue;
+                }
+                else
+                    break;
+            }
+
+            break;
+            
+        }
+        case 2:
+        {
+            cout << "Under devloped! comeback late\n";
+            break;
+        }
+        case 4:
+        {
+            cout << "Under devloped! comeback late\n";
+            break;
+        }
+        case 5:
+        {
+            cout << "Under devloped! comeback late\n";
+            break;
+        }
+
+        }
+    }
+   
+   
+
+}
